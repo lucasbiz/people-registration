e03ee73ca15a17f42dbcc0028a51aaa0b3632fc0
lucasbizt <lucaasbizteixeira@gmail.com>
2025-08-25 13:00:40 -0300

    style: remove unused CSS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,2 +1,2 @@
-export const baseApiUrl = 'https://dev-api-plt.4asset.net.br/exam/v1/persons';
-// export const baseApiUrl: string = 'http://localhost:3000/api/persons';
+// export const baseApiUrl = 'https://dev-api-plt.4asset.net.br/exam/v1/persons';
+export const baseApiUrl = 'http://localhost:3000/v1/persons';
